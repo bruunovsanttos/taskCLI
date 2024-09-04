a04bb319c9e3cb21751d15bef06e3975d9c4a0c0
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-09-04 20:01:12 -0300

    aprendendo os conceitos das bibliotacas a serem utilizadas no sistema

---
+++ taskCLI.py
@@ -0,0 +1,18 @@
+import os
+import json
+from datetime import datetime
+
+
+data = datetime.now()
+data_str_data = "%d/%m/%y"
+data_str_fmt = "%d/%m/%Y %H:%M:%S"
+print(data)
+
+#formato_hora = hora_momento.strftime("%dd %mm %yy %H:%M")
+
+#as para nomear o arquivo como quiser
+
+#with open("task.json", "w", encoding="utf-8") as arquivo:
+#    tarefas = json.load(arquivo)
+
+
